2a8a86c59c7d5101634640e196800293d74567c2
lparis42 <56311304+lparis42@users.noreply.github.com>
2024-03-21 14:55:50 +0100

    k

--- server/src/server.js
+++ server/src/server.js
@@ -10,20 +10,13 @@ class Server {
     constructor() {
         this.app = express();
         this.server = http.createServer(this.app);
-        this.io = socketIo(this.server, {
-            cors: {
-                origin: ["http://localhost:3000"/*, "http://red-tetris.ddns.net"*/],
-                methods: ["GET", "POST"]
-            }
-        });
-        this.gameManager = new GameManager(this.io);
         this.configureMiddleware();
         this.configureRoutes();
         this.start();
     }
 
     configureMiddleware() {
-        // Middleware pour autoriser les requêtes CORS
+        // Middleware pour autoriser les requêtes CORS et initialiser socket.io et GameManager
         this.app.use((req, res, next) => {
             // Autoriser les requêtes depuis les domaines spécifiés
             res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'/*, 'http://red-tetris.ddns.net'*/);
@@ -31,12 +24,29 @@ class Server {
             res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
             // Autoriser les en-têtes spécifiés
             res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-            // Poursuivre vers la prochaine étape du middleware
+            
+            // Initialiser socket.io
+            const io = require('socket.io')(this.server, {
+                cors: {
+                    origin: ["http://localhost:3000"/*, "http://red-tetris.ddns.net"*/],
+                    methods: ["GET", "POST"]
+                }
+            });
+            
+            // Ajouter l'objet io à l'objet de la requête
+            req.io = io;
+    
+            // Initialiser le GameManager avec l'instance de socket.io
+            req.gameManager = new GameManager(io);
+    
+            // Passer la requête au prochain middleware
             next();
         });
-
+    
+        // Middleware pour servir les fichiers statiques
         this.app.use(express.static(path.join(__dirname, '../..', 'client', 'build')));
     }
+    
 
     configureRoutes() {
         this.app.get('/', (req, res) => {
