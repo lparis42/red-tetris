96245bad910e804f313493aa52325dc82b736e7f
lparis42 <56311304+lparis42@users.noreply.github.com>
2024-04-07 16:26:39 +0200

    k

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = function override(config, env) {
   if (env === 'production') {
     config.output.filename = 'static/js/bundle.js';
+    config.plugins[5].options.filename = 'static/css/bundle.css';
   }
 
   config.output.path = path.resolve(__dirname, 'build');
